a802517aead89f6af6272224627568e6b5257467
Tirza <Tirza0556707982@gmail.com>
2024-03-11 18:19:36 +0200

    HW-10

---
+++ 10/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Course = require('./controller/courses.js');
+const Worker = require('./controller/workers.js');
+const port = 3000;
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/Courses',Course);
+app.use('/Workers',Worker);
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('router error');
+//   });
+  
+  // app.delete("*", (req, res) => {  
+  //   res.status(404).send("PAGE NOT FOUND");
+  // }); 
+  // app.get("*", (req, res) => {  
+  //   res.status(404).send("PAGE NOT FOUND");
+  // }); 
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
